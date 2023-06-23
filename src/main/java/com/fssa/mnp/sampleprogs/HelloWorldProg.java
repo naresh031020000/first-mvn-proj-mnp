a2107aff5a74914508714f437e6d42384d40a175
naresh031020000 <116252192+naresh031020000@users.noreply.github.com>
2023-06-23 10:20:45 +0530

    my-first-maven

---
+++ src/main/java/com/fssa/mnp/sampleprogs/HelloWorldProg.java
@@ -0,0 +1,12 @@
+package com.fssa.mnp.sampleprogs;
+
+public class HelloWorldProg {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Hello World");
+
+	}
+
+}
