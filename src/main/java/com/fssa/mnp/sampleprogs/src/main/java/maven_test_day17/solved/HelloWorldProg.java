8db3305876d34a48943d5c27f4f3b7a0280048dc
naresh031020000 <116252192+naresh031020000@users.noreply.github.com>
2023-06-23 10:52:13 +0530

    helloTest_updated

---
+++ src/main/java/com/fssa/mnp/sampleprogs/src/main/java/maven_test_day17/solved/HelloWorldProg.java
@@ -0,0 +1,12 @@
+package maven_test_day17.solved;
+
+public class HelloWorldProg {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Hello World");
+
+	}
+
+}
